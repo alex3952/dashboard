90ef445fd705c1824e94b84bcaf4c29405455cac
alex3952 <alex1234440@gmail.com>
2024-06-19 10:40:57 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 //import { useState } from 'react'
 import Grid from '@mui/material/Unstable_Grid2';
 import Indicator from './components/Indicator';
+import Summary from './components/Summary';
+import BasicTable from './components/BasicTable';
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 import './App.css'
@@ -12,14 +14,31 @@ function App() {
 
   return (
     <Grid container spacing={5}>
-	      <Grid xs={12} sm={4} md={3} lg={2}>1</Grid>
-	      <Grid xs={6} sm={4} md={3} lg={2}>2</Grid>
-	      <Grid xs={6} sm={4} md={3} lg={2}>3</Grid>
-	      <Grid xs={12} sm={4} md={3} lg={2}>4</Grid>
-	      <Grid xs={6} sm={4} md={6} lg={2}>5</Grid>
-	      <Grid xs={6} sm={4} md={6} lg={2}>6</Grid>
-		  <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+	      <Grid xs={6} sm={4} md={3} lg={4}>
+			<Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+		  </Grid>
+	      <Grid xs={6} sm={4} md={3} lg={4}>
+			<Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+		  </Grid>
+	      <Grid xs={6} sm={4} md={3} lg={4}>
+			<Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+		  </Grid>
+	      <Grid xs={6} sm={4} md={3} lg={6}>
+			<Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+		  </Grid>
+	      <Grid xs={6} sm={4} md={3} lg={6}>
+			<Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+		  </Grid>
+		  <Grid xs={12} sm={4} md={3} lg={4}>
+			<Summary></Summary>
+		  </Grid>
+		  <Grid xs={6} sm={4} md={3} lg={8}>
+			<BasicTable></BasicTable>
+		  </Grid>
+		  
+		  
 	</Grid>
+	
   )
 }
 
